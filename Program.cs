77158abdbab3769d50d73e43e63c8cdf839f2e13
Lucy Laurentino <9998329@BCS.NET.NZ>
2017-10-24 10:53:59 +1300

    added comments

--- Program.cs
+++ Program.cs
@@ -5,9 +5,10 @@ namespace _9998329_assessment3_question01
 {
     class Program
     {
+        //Creating a list for falconers and falcons
         static List<string> falconers = new List<string>{"Debbie", "Heidi", "Noel", "Melissa", "Kirsten", "Koshy"};
-        static List<string> falcons = new List<string>{"Hisan", "Ka Kite", "Siren", "Koa", "Stella", "Mojo"};
-        static void Main(string[] args)
+        static List<string> falcons = new List<string>{"Hisan", "Ka Kite", "Siren", "Koa", "Stella", "Mojo"};    
+        static void Main(string[] args) //method
         {
            
            //Start the program with Clear();
@@ -28,13 +29,14 @@ namespace _9998329_assessment3_question01
            
            
         }
+        //Generating random number
         static int GenerateRandomNumber(List<string> myList)
         {
             var rand = new Random();
             return rand.Next(1, myList.Count);
         }
         static string DisplayFalconer()
-        {
+        {   //declaring variables for falcons and falconers
             var selectFalcons = falcons[GenerateRandomNumber(falcons)];
             var selectFalconers = falconers[GenerateRandomNumber(falconers)];
             return $"Selected Falconer = {selectFalconers}\nSelected Falcon ={selectFalcons}";
