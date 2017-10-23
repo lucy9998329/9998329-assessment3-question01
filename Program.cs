29dcf964aaa308221af1bcd35d981f1f2523e576
Lucy Laurentino <9998329@BCS.NET.NZ>
2017-10-24 10:45:35 +1300

    finished program

--- Program.cs
+++ Program.cs
@@ -1,20 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _9998329_assessment3_question01
 {
     class Program
     {
+        static List<string> falconers = new List<string>{"Debbie", "Heidi", "Noel", "Melissa", "Kirsten", "Koshy"};
+        static List<string> falcons = new List<string>{"Hisan", "Ka Kite", "Siren", "Koa", "Stella", "Mojo"};
         static void Main(string[] args)
         {
            
            //Start the program with Clear();
-           Console.Clear();
-           
+           Console.Clear();           
 
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(DisplayFalconer());
+            Console.WriteLine(DisplayFalconer());
+            Console.WriteLine(DisplayFalconer());
+            Console.WriteLine(DisplayFalconer());
+            Console.WriteLine(DisplayFalconer());
+            Console.WriteLine(DisplayFalconer());
 
-            
-           
            //End the program with blank line and instructions
            Console.ResetColor();
            Console.WriteLine();
@@ -22,6 +27,17 @@ namespace _9998329_assessment3_question01
            Console.ReadKey();
            
            
+        }
+        static int GenerateRandomNumber(List<string> myList)
+        {
+            var rand = new Random();
+            return rand.Next(1, myList.Count);
+        }
+        static string DisplayFalconer()
+        {
+            var selectFalcons = falcons[GenerateRandomNumber(falcons)];
+            var selectFalconers = falconers[GenerateRandomNumber(falconers)];
+            return $"Selected Falconer = {selectFalconers}\nSelected Falcon ={selectFalcons}";
         }
     }
 }
